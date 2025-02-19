07ae286895dc007ed47a417511fafd38050d600b
Pol Ramos <119877499+pramos-m@users.noreply.github.com>
2025-02-19 20:27:39 +0100

    loginpage styles working

--- tradesim/tradesim/pages/login.py
+++ tradesim/tradesim/pages/login.py
@@ -1,346 +1,225 @@
-# import reflex as rx
-# from ..components.layout import layout
-# from ..state.auth_state import AuthState
 
-# # Constantes para las imágenes
-# BACKGROUND_IMAGE = "/background.png"
-# LOGO_IMAGE = "/logo.svg"
-
-# def login_form() -> rx.Component:
-#     """Componente del formulario de login."""
-#     return rx.box(
-#         rx.vstack(
-#             rx.heading("Iniciar Sesión", size="9", mb="6"),
-#             rx.vstack(
-#                 rx.box(
-#                     rx.text("EMAIL", font_size="1", font_weight="500", color="black"),
-#                     rx.input(
-#                         placeholder="ElonMusk@iCloud.com",
-#                         value=AuthState.email,
-#                         on_change=AuthState.set_email,
-#                         bg="gray.100",
-#                         border="0px",
-#                         size="3",
-#                         padding="6",
-#                     ),
-#                     width="100%",
-#                 ),
-#                 rx.box(
-#                     rx.text("CONTRASEÑA", font_size="1", font_weight="500", color="black"),
-#                     rx.input(
-#                         type_="password",
-#                         value=AuthState.password,
-#                         on_change=AuthState.set_password,
-#                         bg="gray.100",
-#                         border="0px",
-#                         size="3",
-#                         padding="6",
-#                     ),
-#                     width="100%",
-#                 ),
-#                 spacing="4",
-#                 width="100%",
-#             ),
-#             rx.button(
-#                 "Iniciar Sesión",
-#                 on_click=AuthState.login,
-#                 width="100%",
-#                 bg="blue.500",
-#                 color="white",
-#                 size="3",
-#                 _hover={"bg": "blue.600"},
-#             ),
-#             rx.link(
-#                 "¿No tienes cuenta? Regístrate",
-#                 on_click=AuthState.set_active_tab("register"),
-#                 color="blue.500",
-#                 text_align="center",
-#                 font_size="1",
-#             ),
-#             spacing="6",
-#             width="100%",
-#         ),
-#         display=rx.cond(AuthState.active_tab == "login", "block", "none"),
-#         bg="transparent",  # Cambiado a transparente
-#         padding="8",
-#         border_radius="lg",
-#         box_shadow="lg",
-#     )
-
-# def register_form() -> rx.Component:
-#     """Componente del formulario de registro."""
-#     return rx.box(
-#         rx.vstack(
-#             rx.heading("Registro", size="9", mb="6"),
-#             rx.vstack(
-#                 rx.box(
-#                     rx.text("NOMBRE", font_size="1", font_weight="500", color="black"),
-#                     rx.input(
-#                         placeholder="Elon Musk",
-#                         value=AuthState.username,
-#                         on_change=AuthState.set_username,
-#                         bg="gray.100",
-#                         border="0px",
-#                         size="3",
-#                         padding="6",
-#                     ),
-#                     width="100%",
-#                 ),
-#                 rx.box(
-#                     rx.text("EMAIL", font_size="1", font_weight="500", color="black"),
-#                     rx.input(
-#                         placeholder="ElonMusk@iCloud.com",
-#                         value=AuthState.email,
-#                         on_change=AuthState.set_email,
-#                         bg="gray.100",
-#                         border="0px",
-#                         size="3",
-#                         padding="6",
-#                     ),
-#                     width="100%",
-#                 ),
-#                 rx.box(
-#                     rx.text("CONTRASEÑA", font_size="1", font_weight="500", color="black"),
-#                     rx.input(
-#                         type_="password",
-#                         value=AuthState.password,
-#                         on_change=AuthState.set_password,
-#                         bg="gray.100",
-#                         border="0px",
-#                         size="3",
-#                         padding="6",
-#                     ),
-#                     width="100%",
-#                 ),
-#                 rx.box(
-#                     rx.text("CONFIRMAR CONTRASEÑA", font_size="1", font_weight="500", color="black"),
-#                     rx.input(
-#                         type_="password",
-#                         value=AuthState.confirm_password,
-#                         on_change=AuthState.set_confirm_password,
-#                         bg="gray.100",
-#                         border="0px",
-#                         size="3",
-#                         padding="6",
-#                     ),
-#                     width="100%",
-#                 ),
-#                 spacing="4",
-#                 width="100%",
-#             ),
-#             rx.button(
-#                 "Registrarse",
-#                 on_click=AuthState.register,
-#                 width="100%",
-#                 bg="blue.500",
-#                 color="white",
-#                 size="3",
-#                 _hover={"bg": "blue.600"},
-#             ),
-#             rx.link(
-#                 "¿Ya tienes cuenta? Inicia Sesión",
-#                 on_click=AuthState.set_active_tab("login"),
-#                 color="blue.500",
-#                 text_align="center",
-#                 font_size="1",
-#             ),
-#             spacing="6",
-#             width="100%",
-#         ),
-#         display=rx.cond(AuthState.active_tab == "register", "block", "none"),
-#         bg="transparent",  # Cambiado a transparente
-#         padding="8",
-#         border_radius="lg",
-#         box_shadow="lg",
-#     )
-
-# def login_form() -> rx.Component:
-#     """Componente del formulario de login."""
-#     return rx.box(
-#         rx.vstack(
-#             rx.heading("Iniciar Sesión", size="9", mb="6"),
-#             # Add error message display
-#             rx.cond(
-#                 AuthState.error_message,
-#                 rx.text(
-#                     AuthState.error_message,
-#                     color="red.500",
-#                     font_size="sm",
-#                     text_align="center",
-#                 ),
-#             ),
-#             rx.vstack(
-#                 rx.box(
-#                     rx.text("EMAIL", font_size="1", font_weight="500", color="black"),
-#                     rx.input(
-#                         placeholder="ElonMusk@iCloud.com",
-#                         value=AuthState.email,
-#                         on_change=AuthState.set_email,
-#                         bg="gray.100",
-#                         border="0px",
-#                         size="3",
-#                         padding="6",
-#                     ),
-#                     width="100%",
-#                 ),
-#                 rx.box(
-#                     rx.text("CONTRASEÑA", font_size="1", font_weight="500", color="black"),
-#                     rx.input(
-#                         type_="password",
-#                         value=AuthState.password,
-#                         on_change=AuthState.set_password,
-#                         bg="gray.100",
-#                         border="0px",
-#                         size="3",
-#                         padding="6",
-#                     ),
-#                     width="100%",
-#                 ),
-#                 spacing="4",
-#                 width="100%",
-#             ),
-#             rx.button(
-#                 "Iniciar Sesión",
-#                 on_click=AuthState.login,
-#                 width="100%",
-#                 bg="blue.500",
-#                 color="white",
-#                 size="3",
-#                 _hover={"bg": "blue.600"},
-#             ),
-#             rx.link(
-#                 "¿No tienes cuenta? Regístrate",
-#                 on_click=AuthState.set_active_tab("register"),
-#                 color="blue.500",
-#                 text_align="center",
-#                 font_size="1",
-#             ),
-#             spacing="6",
-#             width="100%",
-#         ),
-#         display=rx.cond(AuthState.active_tab == "login", "block", "none"),
-#         bg="transparent",
-#         padding="8",
-#         border_radius="lg",
-#         box_shadow="lg",
-#     )
+# gradient color for fields "#555555"
+# botones tipo no tienes cuenta? "#5271FF"
 
 import reflex as rx
 from ..state.auth_state import AuthState
 from ..utils.auth_middleware import public_only
 
+# Constantes para las imágenes
+BACKGROUND_IMAGE = "./background.png"
+LOGO_IMAGE = "./logo.svg"
+
 def login_form() -> rx.Component:
-    """Login form component."""
-    return rx.vstack(
-        rx.heading("Iniciar Sesión", size="4"),
-        rx.input(
-            placeholder="Email",
-            value=AuthState.email,
-            on_change=AuthState.set_email,
-            type_="email",
-        ),
-        rx.input(
-            placeholder="Contraseña",
-            value=AuthState.password,
-            on_change=AuthState.set_password,
-            type_="password",
-        ),
-        rx.button(
-            "Iniciar Sesión",
-            on_click=AuthState.login,
+    """Componente del formulario de login."""
+    return rx.box(
+        rx.vstack(
+            rx.heading("Iniciar Sesión", size="9", mb="6"),
+            rx.cond(
+                AuthState.error_message != "",
+                rx.text(
+                    AuthState.error_message,
+                    color="#5271FF",
+                    font_size="sm",
+                    text_align="center",
+                ),
+            ),
+            rx.vstack(
+                rx.box(
+                    rx.text("EMAIL", font_size="1", font_weight="500", color="black"),
+                    rx.input(
+                        placeholder="ElonMusk@iCloud.com",
+                        value=AuthState.email,
+                        on_change=AuthState.set_email,
+                        bg="gray.100",
+                        border="0px",
+                        size="3",
+                        padding="6",
+                    ),
+                    width="100%",
+                ),
+                rx.box(
+                    rx.text("CONTRASEÑA", font_size="1", font_weight="500", color="black"),
+                    rx.input(
+                        type_="password",
+                        value=AuthState.password,
+                        on_change=AuthState.set_password,
+                        bg="gray.100",
+                        border="0px",
+                        size="3",
+                        padding="6",
+                    ),
+                    width="100%",
+                ),
+                spacing="4",
+                width="100%",
+            ),
+            rx.button(
+                "Iniciar Sesión",
+                on_click=AuthState.login,
+                width="100%",
+                bg="#5271FF",
+                color="white",
+                size="3",
+                _hover={"bg": "blue.600"},
+                is_loading=AuthState.loading,
+            ),
+            rx.link(
+                "¿No tienes cuenta? Regístrate",
+                on_click=lambda: AuthState.set_active_tab("register"),
+                color="#5271FF",
+                text_align="center",
+                font_size="1",
+            ),
+            spacing="6",
             width="100%",
-            color_scheme="blue",
-            is_loading=AuthState.loading,
         ),
-        rx.text(
-            "¿No tienes cuenta? Regístrate",
-            color="blue.500",
-            cursor="pointer",
-            on_click=lambda: AuthState.switch_tab("register"),
-            text_align="center",
-        ),
-        width="100%",
-        spacing="4",
+        bg="transparent",
+        padding="8",
+        border_radius="lg",
+        box_shadow="lg",
     )
 
 def register_form() -> rx.Component:
-    """Registration form component."""
-    return rx.vstack(
-        rx.heading("Registro", size="4"),
-        rx.input(
-            placeholder="Nombre de usuario",
-            value=AuthState.username,
-            on_change=AuthState.set_username,
-        ),
-        rx.input(
-            placeholder="Email",
-            value=AuthState.email,
-            on_change=AuthState.set_email,
-            type_="email",
-        ),
-        rx.input(
-            placeholder="Contraseña",
-            value=AuthState.password,
-            on_change=AuthState.set_password,
-            type_="password",
-        ),
-        rx.input(
-            placeholder="Confirmar Contraseña",
-            value=AuthState.confirm_password,
-            on_change=AuthState.set_confirm_password,
-            type_="password",
-        ),
-        rx.button(
-            "Registrarse",
-            on_click=AuthState.register,
+    """Componente del formulario de registro."""
+    return rx.box(
+        rx.vstack(
+            rx.heading("Registro", size="9", mb="6"),
+            rx.cond(
+                AuthState.error_message != "",
+                rx.text(
+                    AuthState.error_message,
+                    color="red.500",
+                    font_size="sm",
+                    text_align="center",
+                ),
+            ),
+            rx.vstack(
+                rx.box(
+                    rx.text("NOMBRE", font_size="1", font_weight="500", color="black"),
+                    rx.input(
+                        placeholder="Elon Musk",
+                        value=AuthState.username,
+                        on_change=AuthState.set_username,
+                        bg="gray.100",
+                        border="0px",
+                        size="3",
+                        padding="6",
+                    ),
+                    width="100%",
+                ),
+                rx.box(
+                    rx.text("EMAIL", font_size="1", font_weight="500", color="black"),
+                    rx.input(
+                        placeholder="ElonMusk@iCloud.com",
+                        value=AuthState.email,
+                        on_change=AuthState.set_email,
+                        bg="gray.100",
+                        border="0px",
+                        size="3",
+                        padding="6",
+                    ),
+                    width="100%",
+                ),
+                rx.box(
+                    rx.text("CONTRASEÑA", font_size="1", font_weight="500", color="black"),
+                    rx.input(
+                        type_="password",
+                        value=AuthState.password,
+                        on_change=AuthState.set_password,
+                        bg="gray.100",
+                        border="0px",
+                        size="3",
+                        padding="6",
+                    ),
+                    width="100%",
+                ),
+                rx.box(
+                    rx.text("CONFIRMAR CONTRASEÑA", font_size="1", font_weight="500", color="black"),
+                    rx.input(
+                        type_="password",
+                        value=AuthState.confirm_password,
+                        on_change=AuthState.set_confirm_password,
+                        bg="gray.100",
+                        border="0px",
+                        size="3",
+                        padding="6",
+                    ),
+                    width="100%",
+                ),
+                spacing="4",
+                width="100%",
+            ),
+            rx.button(
+                "Registrarse",
+                on_click=AuthState.register,
+                width="100%",
+                bg="#5271FF",
+                color="white",
+                size="3",
+                _hover={"bg": "blue.600"},
+                is_loading=AuthState.loading,
+            ),
+            rx.link(
+                "¿Ya tienes cuenta? Inicia Sesión",
+                on_click=lambda: AuthState.set_active_tab("login"),
+                color="#5271FF",
+                text_align="center",
+                font_size="1",
+            ),
+            spacing="6",
             width="100%",
-            color_scheme="blue",
-            is_loading=AuthState.loading,
         ),
-        rx.text(
-            "¿Ya tienes cuenta? Inicia Sesión",
-            color="blue.500",
-            cursor="pointer",
-            on_click=lambda: AuthState.switch_tab("login"),
-            text_align="center",
-        ),
-        width="100%",
-        spacing="4",
+        bg="transparent",
+        padding="8",
+        border_radius="lg",
+        box_shadow="lg",
     )
 
 @public_only
 def login_page() -> rx.Component:
-    """Login/Register page."""
-    return rx.center(
-        rx.vstack(
-            rx.image(src="/logo.svg", height="40px"),
+    """Página principal de login."""
+    return rx.box(
+        # Contenedor principal con fondo
+        rx.box(
+            rx.image(
+                src=BACKGROUND_IMAGE,
+                position="fixed",
+                top="0",
+                left="0",
+                width="100vw",
+                height="100vh",
+                object_fit="cover",
+                z_index="-1",
+                border="2px solid red",  # Borde temporal para depuración
+            ),
+        ),
+        # Logo en la esquina superior izquierda
+        rx.box(
+            rx.image(src="/logo.svg", height="200px"),
+            position="absolute",
+            top="0",
+            left="20px",
+        ),
+        # Contenedor del formulario a la derecha
+        rx.hstack(
+            rx.spacer(),  # Esto empuja el contenido hacia la derecha
             rx.box(
-                rx.cond(
-                    AuthState.error_message != "",
-                    rx.text(
-                        AuthState.error_message,
-                        color="red.500",
-                        margin_bottom="4",
-                    ),
-                ),
                 rx.cond(
                     AuthState.active_tab == "login",
                     login_form(),
                     register_form(),
                 ),
-                padding="6",
-                bg="white",
-                border_radius="lg",
-                box_shadow="lg",
-                width="100%",
-                max_width="400px",
+                width="50%",
+                height="100vh",
+                display="flex",
+                align_items="center",
+                justify_content="center",
             ),
-            spacing="8",
-            width="100%",
         ),
-        padding_y="8",
-        width="100%",
-        min_height="100vh",
+        width="100vw",
+        height="100vh",
+        position="relative",
         background="url('/background.png')",
         background_size="cover",
     )
